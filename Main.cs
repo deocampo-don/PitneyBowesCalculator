b53a05b52ed0c1c8a8c3b07c84204ef2da4ff777
deoca <deoca@DESKTOP-PFEQCH4>
2026-01-11 11:24:17 +0800

    test2

--- Main.cs
+++ Main.cs
@@ -241,7 +241,10 @@ namespace WindowsFormsApp1
             }
         }
 
- 
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            //test
+        }
     }
 }
 
