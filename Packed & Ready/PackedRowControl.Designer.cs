c44abe5533bea36b7edccf400efd5fa67a7145a3
Brent RIVERA <brentriv@corp.idemia.com>
2026-01-22 15:44:53 +0800

    Added packed and ready folder

---
+++ Packed & Ready/PackedRowControl.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace WindowsFormsApp1.Packed___Ready
+{
+    partial class PackedRowControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtStatus = new Krypton.Toolkit.KryptonLabel();
+            this.txtPackDate = new Krypton.Toolkit.KryptonLabel();
+            this.chkboxStatus = new System.Windows.Forms.CheckBox();
+            this.txtJobNumber = new Krypton.Toolkit.KryptonLabel();
+            this.txtPBJob = new Krypton.Toolkit.KryptonLabel();
+            this.lblPackDate = new Krypton.Toolkit.KryptonLabel();
+            this.lblTrays = new Krypton.Toolkit.KryptonLabel();
+            this.txtTrays = new Krypton.Toolkit.KryptonLabel();
+            this.lblEnvQty = new Krypton.Toolkit.KryptonLabel();
+            this.txtEnvelopeQty = new Krypton.Toolkit.KryptonLabel();
+            this.lblPallets = new Krypton.Toolkit.KryptonLabel();
+            this.txtPallets = new Krypton.Toolkit.KryptonLabel();
+            this.btnView = new Krypton.Toolkit.KryptonButton();
+            this.SuspendLayout();
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtStatus.Location = new System.Drawing.Point(414, 78);
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.Size = new System.Drawing.Size(53, 24);
+            this.txtStatus.StateCommon.ShortText.Color1 = System.Drawing.Color.Black;
+            this.txtStatus.StateCommon.ShortText.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtStatus.TabIndex = 27;
+            this.txtStatus.Values.Text = "Status";
+            // 
+            // txtPackDate
+            // 
+            this.txtPackDate.Location = new System.Drawing.Point(358, 247);
+            this.txtPackDate.Name = "txtPackDate";
+            this.txtPackDate.Size = new System.Drawing.Size(90, 24);
+            this.txtPackDate.StateCommon.ShortText.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPackDate.TabIndex = 26;
+            this.txtPackDate.Values.Text = "01/30/2026";
+            // 
+            // chkboxStatus
+            // 
+            this.chkboxStatus.AutoSize = true;
+            this.chkboxStatus.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.chkboxStatus.Location = new System.Drawing.Point(449, 16);
+            this.chkboxStatus.Name = "chkboxStatus";
+            this.chkboxStatus.Size = new System.Drawing.Size(18, 17);
+            this.chkboxStatus.TabIndex = 25;
+            this.chkboxStatus.UseVisualStyleBackColor = true;
+            // 
+            // txtJobNumber
+            // 
+            this.txtJobNumber.Location = new System.Drawing.Point(38, 78);
+            this.txtJobNumber.Name = "txtJobNumber";
+            this.txtJobNumber.Size = new System.Drawing.Size(20, 24);
+            this.txtJobNumber.StateCommon.ShortText.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtJobNumber.TabIndex = 24;
+            this.txtJobNumber.Values.Text = "0";
+            // 
+            // txtPBJob
+            // 
+            this.txtPBJob.Location = new System.Drawing.Point(35, 49);
+            this.txtPBJob.Name = "txtPBJob";
+            this.txtPBJob.Size = new System.Drawing.Size(116, 31);
+            this.txtPBJob.StateCommon.ShortText.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPBJob.TabIndex = 23;
+            this.txtPBJob.Values.Text = "CAPONE";
+            // 
+            // lblPackDate
+            // 
+            this.lblPackDate.Location = new System.Drawing.Point(369, 223);
+            this.lblPackDate.Name = "lblPackDate";
+            this.lblPackDate.Size = new System.Drawing.Size(81, 24);
+            this.lblPackDate.StateCommon.ShortText.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPackDate.TabIndex = 22;
+            this.lblPackDate.Values.Text = "Pack Date";
+            // 
+            // lblTrays
+            // 
+            this.lblTrays.Location = new System.Drawing.Point(35, 205);
+            this.lblTrays.Margin = new System.Windows.Forms.Padding(0);
+            this.lblTrays.Name = "lblTrays";
+            this.lblTrays.Size = new System.Drawing.Size(48, 24);
+            this.lblTrays.StateCommon.ShortText.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTrays.TabIndex = 20;
+            this.lblTrays.Values.Text = "Trays";
+            // 
+            // txtTrays
+            // 
+            this.txtTrays.Location = new System.Drawing.Point(28, 223);
+            this.txtTrays.Name = "txtTrays";
+            this.txtTrays.Size = new System.Drawing.Size(40, 48);
+            this.txtTrays.StateCommon.ShortText.Font = new System.Drawing.Font("Arial", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTrays.TabIndex = 21;
+            this.txtTrays.Values.Text = "0";
+            // 
+            // lblEnvQty
+            // 
+            this.lblEnvQty.Location = new System.Drawing.Point(34, 120);
+            this.lblEnvQty.Margin = new System.Windows.Forms.Padding(0);
+            this.lblEnvQty.Name = "lblEnvQty";
+            this.lblEnvQty.Size = new System.Drawing.Size(105, 24);
+            this.lblEnvQty.StateCommon.ShortText.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEnvQty.TabIndex = 18;
+            this.lblEnvQty.Values.Text = "Envelope Qty";
+            // 
+            // txtEnvelopeQty
+            // 
+            this.txtEnvelopeQty.LabelStyle = Krypton.Toolkit.LabelStyle.TitleControl;
+            this.txtEnvelopeQty.Location = new System.Drawing.Point(28, 139);
+            this.txtEnvelopeQty.Name = "txtEnvelopeQty";
+            this.txtEnvelopeQty.Size = new System.Drawing.Size(40, 48);
+            this.txtEnvelopeQty.StateCommon.ShortText.Font = new System.Drawing.Font("Arial", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEnvelopeQty.TabIndex = 19;
+            this.txtEnvelopeQty.Values.Text = "0";
+            // 
+            // lblPallets
+            // 
+            this.lblPallets.Location = new System.Drawing.Point(393, 120);
+            this.lblPallets.Name = "lblPallets";
+            this.lblPallets.Size = new System.Drawing.Size(57, 24);
+            this.lblPallets.StateCommon.ShortText.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPallets.TabIndex = 16;
+            this.lblPallets.Values.Text = "Pallets";
+            // 
+            // txtPallets
+            // 
+            this.txtPallets.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPallets.Location = new System.Drawing.Point(408, 140);
+            this.txtPallets.Name = "txtPallets";
+            this.txtPallets.Size = new System.Drawing.Size(40, 48);
+            this.txtPallets.StateCommon.ShortText.Font = new System.Drawing.Font("Arial", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPallets.TabIndex = 17;
+            this.txtPallets.Values.Text = "0";
+            // 
+            // btnView
+            // 
+            this.btnView.ButtonStyle = Krypton.Toolkit.ButtonStyle.NavigatorStack;
+            this.btnView.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnView.Location = new System.Drawing.Point(318, 286);
+            this.btnView.Name = "btnView";
+            this.btnView.Size = new System.Drawing.Size(149, 36);
+            this.btnView.StateNormal.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(103)))), ((int)(((byte)(80)))), ((int)(((byte)(164)))));
+            this.btnView.StateNormal.Border.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(103)))), ((int)(((byte)(80)))), ((int)(((byte)(164)))));
+            this.btnView.StateNormal.Border.DrawBorders = ((Krypton.Toolkit.PaletteDrawBorders)((((Krypton.Toolkit.PaletteDrawBorders.Top | Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.btnView.StateNormal.Border.Rounding = 5F;
+            this.btnView.StateNormal.Border.Width = 1;
+            this.btnView.StateNormal.Content.ShortText.Color1 = System.Drawing.Color.White;
+            this.btnView.StateNormal.Content.ShortText.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold);
+            this.btnView.StateNormal.Content.ShortText.TextH = Krypton.Toolkit.PaletteRelativeAlign.Center;
+            this.btnView.StatePressed.Back.Color1 = System.Drawing.Color.Black;
+            this.btnView.StatePressed.Border.Rounding = 5F;
+            this.btnView.StatePressed.Border.Width = 1;
+            this.btnView.StateTracking.Back.Color1 = System.Drawing.Color.Black;
+            this.btnView.TabIndex = 15;
+            this.btnView.Values.DropDownArrowColor = System.Drawing.Color.Empty;
+            this.btnView.Values.Text = "View";
+            // 
+            // PackedRowControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.txtPackDate);
+            this.Controls.Add(this.chkboxStatus);
+            this.Controls.Add(this.txtJobNumber);
+            this.Controls.Add(this.txtPBJob);
+            this.Controls.Add(this.lblPackDate);
+            this.Controls.Add(this.lblTrays);
+            this.Controls.Add(this.txtTrays);
+            this.Controls.Add(this.lblEnvQty);
+            this.Controls.Add(this.txtEnvelopeQty);
+            this.Controls.Add(this.lblPallets);
+            this.Controls.Add(this.txtPallets);
+            this.Controls.Add(this.btnView);
+            this.Name = "PackedRowControl";
+            this.Size = new System.Drawing.Size(494, 339);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Krypton.Toolkit.KryptonLabel txtStatus;
+        private Krypton.Toolkit.KryptonLabel txtPackDate;
+        private System.Windows.Forms.CheckBox chkboxStatus;
+        private Krypton.Toolkit.KryptonLabel txtJobNumber;
+        private Krypton.Toolkit.KryptonLabel txtPBJob;
+        private Krypton.Toolkit.KryptonLabel lblPackDate;
+        private Krypton.Toolkit.KryptonLabel lblTrays;
+        private Krypton.Toolkit.KryptonLabel txtTrays;
+        private Krypton.Toolkit.KryptonLabel lblEnvQty;
+        private Krypton.Toolkit.KryptonLabel txtEnvelopeQty;
+        private Krypton.Toolkit.KryptonLabel lblPallets;
+        private Krypton.Toolkit.KryptonLabel txtPallets;
+        private Krypton.Toolkit.KryptonButton btnView;
+    }
+}
